25ed4a3d006619be60e3a1452765c5c5cd45b323
박범준 <bumjunpark@bagbeomjuns-MacBook-Pro.local>
2022-09-09 21:05:37 -0500

    ready for publishing

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="pws_helper",
+    packages=["pws_helper"],
+    version="v.1.0.0-alpha",
+    description="A demo package to convert Environmental Protection Agency's Public Water System IDs to Lat/Long",
+    author="Bumjun Park",
+    author_email="bpark67@wisc.edu",
+    url="https://github.com/bpark67/PWS_Helper",
+    download_url="https://github.com/bpark67/PWS_Helper/archive/refs/tags/v1.0.0-alpha.tar.gz",
+    keywords=[],
+    classifiers=[],
+)
